642c3aa050ef476c4496c3060bf754df61d44d22
Gazi Akter <gaziakterdev@gmail.com>
2024-04-26 22:44:29 +0600

    class-03

--- functions.php
+++ functions.php
@@ -34,39 +34,9 @@ function harry_theme_support(){
 add_action('after_setup_theme','harry_theme_support');
 
 
-// harry css js enqueue 
-function harry_add_theme_scripts() {
-	
-	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'meanmenu', get_template_directory_uri() . '/assets/css/meanmenu.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'animate', get_template_directory_uri() . '/assets/css/animate.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'swiper-bundle', get_template_directory_uri() . '/assets/css/swiper-bundle.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'slick', get_template_directory_uri() . '/assets/css/slick.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'nouislider', get_template_directory_uri() . '/assets/css/nouislider.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'backtotop', get_template_directory_uri() . '/assets/css/backtotop.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'magnific-popup', get_template_directory_uri() . '/assets/css/magnific-popup.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'font-awesome-pro', get_template_directory_uri() . '/assets/css/nice-select.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'elegant-icon', get_template_directory_uri() . '/assets/css/elegant-icon.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'spacing', get_template_directory_uri() . '/assets/css/spacing.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'harry-main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0.0', 'all' );
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'waypoints', get_template_directory_uri() . '/assets/js/waypoints.js', array( 'jquery' ), 1.0, true );
-	wp_enqueue_script( 'bootstrap-bundle', get_template_directory_uri() . '/assets/js/bootstrap-bundle.js', array( '' ), 5.1.3, true );
-	wp_enqueue_script( 'meanmenu', get_template_directory_uri() . '/assets/js/meanmenu.js', array( '' ), 1.0, true );
-	wp_enqueue_script( 'slick', get_template_directory_uri() . '/assets/js/slick.js', array( '' ), 1.1, true );
-	wp_enqueue_script( 'nouislider', get_template_directory_uri() . '/assets/js/nouislider.js', array( '' ), 1.0, true );
-	wp_enqueue_script( 'magnific-popup', get_template_directory_uri() . '/assets/js/magnific-popup.js', array( '' ), 1.1, true );
-	wp_enqueue_script( 'parallax', get_template_directory_uri() . '/assets/js/parallax.js', array( '' ), 1.0, true );
-	wp_enqueue_script( 'backtotop', get_template_directory_uri() . '/assets/js/backtotop.js', array( '' ), 1.0, true );
-	wp_enqueue_script( 'nice-select', get_template_directory_uri() . '/assets/js/nice-select.js', array( '' ), 1.0, true );
-	wp_enqueue_script( 'purecounter', get_template_directory_uri() . '/assets/js/purecounter.js', array( '' ), 1.0, true );
-	wp_enqueue_script( 'wow', get_template_directory_uri() . '/assets/js/wow.js', array( '' ), 1.1, true );
-	wp_enqueue_script( 'isotope-pkgd', get_template_directory_uri() . '/assets/js/isotope-pkgd.js', array( 'imagesloaded' ), 1.1, true );
-	wp_enqueue_script( 'harry-main', get_template_directory_uri() . '/assets/js/main.js', array( '' ), 1.1, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+// harry_header
+function harry_header(){
+	get_template_part( 'template-parts/header/header-1' );
 }
-add_action( 'wp_enqueue_scripts', 'harry_add_theme_scripts' );
+
+include_once('inc/common/scripts.php');
--- template-parts/header/preloader.php
+++ template-parts/header/preloader.php
@@ -1,26 +1,5 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
-   <head>
-      <meta charset="<?php bloginfo( 'charset' ); ?>">
-      <?php if ( is_singular() && pings_open( get_queried_object() ) ): ?>
-      <?php endif;?>
-      <meta http-equiv="x-ua-compatible" content="ie=edge">
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      
 
-      <?php wp_head(); ?> 
-
-
-   </head>
-   <body <?php body_class(); ?>>
-      <!--[if lte IE 9]>
-      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-      <![endif]-->
-      <!-- pre loader area start -->
-
-
-      <div id="loading">
+<div id="loading">
          <div id="loading-center">
             <div id="loading-center-absolute">
                <!-- loading content here -->
@@ -57,6 +36,3 @@
          </button>
       </div>
       <!-- back to top end -->
-
-
-<?php harry_header(); ?>      
